0d2d3c80895e335606ff431f84a3bdcd650490ee
Elard Burga Goicochea <elard3d@hotmail.com>
2020-09-17 23:13:12 -0500

    Crud Listo

--- DIARS_T1/Controllers/PersonaController.cs
+++ DIARS_T1/Controllers/PersonaController.cs
@@ -18,5 +18,70 @@ namespace DIARS_T1.Controllers
 
             return View(_context.Persona.ToList());
         }
+
+
+        [HttpGet]
+        public ActionResult Create() {
+
+            ViewBag.Ciudad = _context.Ciudad.ToList();
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Persona persona)
+        {
+
+            _context.Add(persona);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Persona");
+        }
+
+        [HttpGet]
+        public ActionResult Edit (int id)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Ciudades = _context.Ciudad.ToList();
+
+                ViewBag.BuscarPersona = _context.Persona.Where(o => o.IdPersona == id).FirstOrDefault();
+
+                ViewBag.Persona = _context.Persona.ToList();
+                          
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Persona");
+            }
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(Persona persona)
+        {
+
+            _context.Persona.Update(persona);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+
+            var persona = _context.Persona.Where(o => o.IdPersona == id).FirstOrDefault();
+
+            _context.Persona.Remove(persona);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
