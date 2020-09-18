5a29cc870faef23412892a14380f1194ee72d365
Elard Burga Goicochea <elard3d@hotmail.com>
2020-09-17 23:46:02 -0500

    Final

--- DIARS_T1/Models/Persona.cs
+++ DIARS_T1/Models/Persona.cs
@@ -9,25 +9,25 @@ namespace DIARS_T1.Models
 
         
         public int IdPersona { get; set; }
-       
+        
         public string Nombre { get; set; }
        
         public string Apellido { get; set; }
-       
+        
         public string Dni{ get; set; }
        
         public DateTime? FachaNacimiento { get; set; }
-       
+     
         public string Genero { get; set; }
-        
+      
         public int? Ciudad { get; set; }
-        
+     
         public string Direccion { get; set; }
        
         public string Correo { get; set; }
        
         public string Usuario { get; set; }
-       
+     
         public string Contraseña { get; set; }
 
         public virtual Ciudad CiudadNavigation { get; set; }
