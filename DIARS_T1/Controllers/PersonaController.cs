ccb10c93d109f955c33b62b9417aff4af04d97d7
Elard Burga Goicochea <elard3d@hotmail.com>
2020-09-17 21:09:55 -0500

    Se creo PersonaController

---
+++ DIARS_T1/Controllers/PersonaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIARS_T1.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
